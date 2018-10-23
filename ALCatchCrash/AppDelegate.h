6f41b6433fb9cf775c325b3bf4be16bd02a6d800
liujiajia <liujiajia@babytree-inc.com>
2018-10-23 22:00:31 +0800

    Initial Commit

---
+++ ALCatchCrash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALCatchCrash
+//
+//  Created by Alan on 23/10/2018.
+//  Copyright © 2018 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALCatchCrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALCatchCrash
+//
+//  Created by Alan on 23/10/2018.
+//  Copyright © 2018 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
